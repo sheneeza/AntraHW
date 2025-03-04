f0f05f8709bce1594ee813cf78e0ffe20665549b
sheneeza <126906225+sheneeza@users.noreply.github.com>
2025-03-04 14:17:33 -0500

    added answers and code for playing with console app

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// Playing With The Console App
 
-Console.WriteLine("Hello, World!");
+/* Too many characters in character literal
+Console.WriteLine('Hello');
+*/
+
+/* The name HelloWorld does not exist in the current context
+Console.WriteLine(HelloWorld);
+*/
+
+string str = "Hello World";
+Console.WriteLine(str);
+
+// Using ReadLine and WriteLine
+Console.Write("Enter favorite color: ");
+string favColor = Console.ReadLine();
+Console.Write("Enter astrology sign: ");
+string astrology = Console.ReadLine();
+Console.Write("Enter address number: ");
+string addr = Console.ReadLine();
+
+Console.WriteLine($"Your hacker name is {favColor}{astrology}{addr}");
